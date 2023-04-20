6e7948e70f2721f87788c1c277f65042efdadd1e
branber <brandonberke@gmail.com>
2023-04-20 00:08:22 -0400

    add isProfileComplete to user

--- src/utils/auth/encryptRefreshToken.ts
+++ src/utils/auth/encryptRefreshToken.ts
@@ -1,13 +1,10 @@
 import { AES } from "crypto-js";
 
 export const encryptRefreshToken = (token: string) => {
-  console.log(`Encrypting this token: ${token}`);
-  console.log(`with: ${process.env.TOKEN_ENCRYPT_SECRET}`);
   const encrypted = AES.encrypt(
     token,
     process.env.TOKEN_ENCRYPT_SECRET!
   ).toString();
-  console.log("ENCRYPTED TOKEN: ", encrypted);
 
   return encrypted;
 };
