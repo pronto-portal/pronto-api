9702712e537e2d94e0cfc7afe90c14676c49c92f
branber <brandonberke@gmail.com>
2023-07-10 16:58:34 -0400

    fix dateToCron function

--- src/utils/helper/dateToCron.ts
+++ src/utils/helper/dateToCron.ts
@@ -4,11 +4,11 @@ export const dateToCron = (dateString: string) => {
   // todo: ensure all date string coming into the backend get converted into utc time
   const dateTime = moment.utc(dateString);
 
-  const day = dateTime.day;
-  const month = dateTime.month;
-  const year = dateTime.year;
-  const hour = dateTime.hour;
-  const minute = dateTime.minute;
+  const day = dateTime.day.toString();
+  const month = dateTime.month.toString();
+  const year = dateTime.year.toString();
+  const hour = dateTime.hour.toString();
+  const minute = dateTime.minute.toString();
 
   const cron = `${minute} ${hour} ${day} ${month} ? ${year}`;
   return cron;
