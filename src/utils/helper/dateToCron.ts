1f348eb878bf34e53c473be3215dc2abdca2b2ac
branber <brandonberke@gmail.com>
2023-07-11 00:43:29 -0400

    fix month offset and date

--- src/utils/helper/dateToCron.ts
+++ src/utils/helper/dateToCron.ts
@@ -4,8 +4,8 @@ export const dateToCron = (dateString: string) => {
   // todo: ensure all date string coming into the backend get converted into utc time
   const dateTime = moment.utc(dateString);
 
-  const day = dateTime.day();
-  const month = dateTime.month();
+  const day = dateTime.date();
+  const month = dateTime.month() + 1;
   const year = dateTime.year();
   const hour = dateTime.hour();
   const minute = dateTime.minute();
@@ -18,7 +18,7 @@ export const dateToCron = (dateString: string) => {
   console.log("year", year);
   console.log("hour", hour);
   console.log("minute", minute);
-
+  // min hr day_of_month month day_of_week year
   const cron = `${minute} ${hour} ${day} ${month} ? ${year}`;
   return cron;
 };
