8d0dacd8c0c2652282c61e7f84eeb2baa7858b96
branber <brandonberke@gmail.com>
2023-11-22 20:13:42 -0500

    improve signout route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import prisma from "../datasource/datasource";
 import { authenticate } from "../utils/auth/authenticate";
+import getAuthPayload from "../utils/auth/getAuthPayload";
 
 const authRouter = Router();
 
@@ -13,18 +14,42 @@ interface LoginInput {
 
 authRouter.post("/login", async (req, res) => {
   const reqBody: LoginInput = req.body;
-  console.log(reqBody);
 
   const user = await authenticate({ req, res, prisma }, reqBody);
-
-  console.log(user);
 
   return res.status(200).json(user);
 });
 
 authRouter.post("/signout", async (req, res) => {
-  res.cookie("x-access-token", "", { maxAge: 0, expires: new Date(0) });
-  res.status(200).json({ message: "Successfully signed out" });
+  try {
+    const decodedToken = JSON.parse(
+      Buffer.from(req.headers.authorization || "", "base64").toString()
+    );
+
+    await prisma.refreshToken
+      .delete({
+        where: {
+          userId: (decodedToken as any).sub,
+        },
+      })
+      .then(() => {
+        console.log("Refresh token deleted for ", decodedToken.id);
+      })
+      .catch((err) => {
+        console.log("Error deleting refresh token", err);
+      });
+
+    res.cookie("x-access-token", "", {
+      maxAge: 0,
+      expires: new Date(0),
+      httpOnly: false,
+      secure: false,
+      sameSite: "none",
+    });
+    return res.status(200).json({ message: "Successfully signed out" });
+  } catch (err) {
+    return res.status(500).json({ message: "Error signing out" });
+  }
 });
 
 export default authRouter;
